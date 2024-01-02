2d87e278202a1669fe9f02e13637fb7072bd927c
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-02 01:43:00 -0500

    Fixed for pylint

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+"""Setup file for building package"""
 from setuptools import setup, find_packages
 
 setup(
-    name='phonetic-fr',
+    name='phonetic_fr',
     version='1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'phonetic-fr = src.__main__:main',
+            'phonetic_fr = src.__main__:main',
         ],
     },
 )
