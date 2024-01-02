2c729ec932e4ced0f7d3b1e5abd72488a03292e9
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-02 03:11:45 -0500

    Fixes for pylint

--- tests/test_pyphonetic_fr.py
+++ tests/test_pyphonetic_fr.py
@@ -1,10 +1,10 @@
-f"""Unit tests for phonetic_fr"""
+"""Unit tests for phonetic_fr"""
 import unittest
 import os
 import sys
 from collections import OrderedDict
- 
- 
+
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 from phonetic_fr import phonetic # pylint: disable=import-error,wrong-import-position
 
@@ -27,16 +27,18 @@ class TestPhonetique(unittest.TestCase):
     def test_phonetique_from_file(self):
         """Unit tests for phonetic_fr"""
         test_cases = self.read_test_cases('tests/test_cases.txt')
-        expected_failures = self.read_test_cases('tests/test_cases_faling.txt')
+        expected_fail = self.read_test_cases('tests/test_cases_faling.txt')
 
         for input_string, expected_output in test_cases.items():
             with self.subTest(input_string=input_string):
                 result = phonetic(input_string)
                 if result != expected_output:
-                    if input_string in expected_failures and result == expected_failures[input_string]:
-                        print(f"{input_string} -> {result} [expecting {expected_output}] IGNORED BECAUSE EXPECTED")
+                    if input_string in expected_fail and result == expected_fail[input_string]:
+                        print(f"{input_string} -> {result} " +
+                              "[expecting {expected_output}] IGNORED BECAUSE EXPECTED")
                     else:
-                        print(f"{input_string} -> {result} [expecting {expected_output}]")
+                        print(f"{input_string} -> {result} " +
+                              "[expecting {expected_output}]")
                         self.assertEqual(result, expected_output)
 
 if __name__ == '__main__':
