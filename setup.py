5c9c052502bac4e9e7d3811545d511fb4db3512b
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-02 21:06:50 -0500

    Added long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 """Setup file for building package"""
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='phonetic_fr',
     version='1.0',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': [
             'phonetic_fr = phonetic_distance.__main__:main',
